0e8d82b7e50a42a144c90c2c6b4ce0f5b195e01b
ValeryMukhin <70387596+ValeryMukhin1711@users.noreply.github.com>
2020-08-29 16:01:41 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# Задача 1
+
+integral_number = int (input("Введите целое число "))
+print ("Введено число " + str (integral_number))
+
+string_var = input("Введите строковые данные ")
+print("Введены данные " + string_var)
+
+float_var = float (input("Введите любое число "))
+print("Было введено число " + str(float_var))
+
+print("Значения всех переменных -" + str(integral_number) + " ; " + string_var + " ; " + str (float_var))
+
