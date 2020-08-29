2490c86d25a4c217d992ea2d21316cce9feac3fb
ValeryMukhin <70387596+ValeryMukhin1711@users.noreply.github.com>
2020-08-29 16:09:26 +0300

    Add files via upload

---
+++ Задача_3.py
@@ -0,0 +1,10 @@
+#задача 3
+n = input ("Пожалуйста, введите натуральное число n   ")
+nn = n + n
+nnn = n + n + n
+
+sum_ = int(n) + int (nn) + int (nnn)
+result = str(sum_)
+print ("Сумма чисел n, nn и nnn равна " + result)
+
+
